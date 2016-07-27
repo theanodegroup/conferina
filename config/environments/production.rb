f9569eac959d02d7727edf98d3411011c56513bb
Stas Ermolai <stasermolai@Edward-iMac.local>
2016-07-27 18:08:33 +0300

    changed texts and mail content

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,20 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    # address: "smtp.gmail.com",
+    # port: 587,
+    # domain: Rails.application.secrets.domain_name,
+    # authentication: "plain",
+    # enable_starttls_auto: true,
+    # user_name: Rails.application.secrets.email_provider_username,
+    # password: Rails.application.secrets.email_provider_password
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }

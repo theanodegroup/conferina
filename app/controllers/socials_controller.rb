95ea6cbc68a7353f7a8f2c0502340616dcca46e4
Stas Ermolai <stasermolai@Edward-iMac.local>
2016-09-05 17:32:38 +0300

    Git Test

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -8,8 +8,7 @@ class SocialsController < ApplicationController
 	end
 
 	private
-  
-	def social_params
+  	def social_params
 		params.require(:social).permit(:website, :facebook, :twitter, :youtube, :event_id)
 	end
 	

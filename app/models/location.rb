84a71835e0c22b53ceb6cd207c309adcca18174d
Mykl Clason <myklclason@gmail.com>
2017-02-20 02:51:52 +0000

    Add notable assocations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,11 @@
 class Location < ActiveRecord::Base
 	mount_uploader :avatar, AvatarUploader
 	mount_uploader :detailed_avatar, DetailedAvatarUploader
-	
+
 	belongs_to :event
 	belongs_to :location_type
 	belongs_to :venue
+
 	has_many :sessions
+	has_many :notes, as: :notable
 end

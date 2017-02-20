9d1ec6f40d113078877342f89ea67133bdd3f282
Mykl Clason <myklclason@gmail.com>
2017-02-20 03:35:29 +0000

    Add favorite spec outline

---
+++ spec/features/favorite_spec.rb
@@ -0,0 +1,12 @@
+# @todo: Add spec. This is just a skeleton spec
+feature 'Favorites' do
+  scenario 'As a user (all roles) I want to retrieve my favorite Persons/Locations/Sessions so that
+            I can quickly access the saved information' do
+    pending "add spec(s) for , #{__FILE__}"
+  end
+  scenario 'As a user (all roles) I want to mark a Person/Location/Session as favorite so that I can
+            easily retrive my favorites in future' do
+    pending "add spec(s) for marking locations/persons/sessions as favorite, #{__FILE__}"
+  end
+
+end

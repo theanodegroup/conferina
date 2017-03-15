5175e87cf404bb7b2d4e0c37703c12499255bbcd
Mykl Clason <myklclason@gmail.com>
2017-03-15 04:12:50 +0000

    Get export and bulk export working

--- app/mailers/exporter.rb
+++ app/mailers/exporter.rb
@@ -1,2 +1,48 @@
 class Exporter < ApplicationMailer
+  default from: "\"Conferina\" <no-reply notifications@conferina.com>"
+
+  def export_notes(note)
+    @note = note
+    @notable = @note.notable
+    @user = @note.user
+
+    mail(
+      to: mail_to,
+      bcc: mail_bcc,
+      subject: 'Your exported notes'
+    )
+  end
+
+  def bulk_export_notes(notes)
+    # @todo: Not efficent
+    require 'csv'
+
+    @notes = notes
+    a_note = @notes.first
+    @notable_type = a_note.notable_type.class
+    @user = a_note.user
+
+    attachments["#{@notable_type}_notes.csv"] = CSV.generate(headers: false) do |csv|
+      @notes.each do |note|
+        csv << [note.notable.name, note.content]
+      end
+    end
+
+    mail(
+      to: mail_to,
+      bcc: mail_bcc,
+      subject: 'Your bulk exported notes'
+    )
+  end
+
+  private
+
+  def mail_to
+    "\"#{@user.name}\" <#{@user.email}>"
+  end
+
+  def mail_bcc
+    []
+  end
+
 end

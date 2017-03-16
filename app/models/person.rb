c191b4f2631aaef36fc8d3c50eb0e88bf8d11faa
Mykl Clason <myklclason@gmail.com>
2017-03-16 10:35:05 +0000

    Add similar persons method to Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,4 +12,15 @@ class Person < ActiveRecord::Base
 
 	has_many :notes, as: :notable
 	has_many :favorites, as: :favorites
+
+	def similar_persons
+		# @todo: Inefficent in serveral ways (possible duplicated work resolved by uniq)
+		tag_ids = self.tags.pluck(:id).uniq # Get this person's tags
+		events = Event.includes(:tags).where(tags: { id: tag_ids }).uniq # Find events for those tags
+		events.map(&:persons).flatten.uniq # Get the list of person's for those events
+	end
+
+	def tags
+		self.event.tags
+	end
 end

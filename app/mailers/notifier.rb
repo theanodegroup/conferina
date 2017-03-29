66cf77c3af7bdb69410d30db375693258b7d65a7
Mykl Clason <myklclason@gmail.com>
2017-03-29 16:54:26 +0000

    Fix update event notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -15,48 +15,54 @@ class Notifier < ApplicationMailer
     @event = event
     @changes = event.changes
 
+    error_message = nil
+
+    puts "@changes: #{@changes.inspect}"
+
     if @changes.empty?
-      puts "Event Updated. Subscriptions not sent: No subscribers"
-      return
-    end
-
-    @tracked_changes_definitions = [
-      # Field Display name           Field Name     Field value
-      ['Description',                :description,  @event.description],
-      ['Call For Papers Due Date',   :call_for,     @event.call_for],
-      ['Paper Accepted Notify Date', :notification, @event.notification],
-      ['Paper Submission Deadline',  :submission,   @event.submission],
-      ['Category',                   :category,     @event.category.category],
-      ['Start Date',                 :start_date,   @event.start_date],
-      ['End Date',                   :end_date,     @event.end_date],
-      ['Address',                    :address,      @event.address],
-    ]
-    tracked_fields = @tracked_changes_definitions.map{ |definition| definition[1] }
-
-    if (tracked_fields & @changes.keys).empty?
-      puts "Event Updated. Subscriptions not sent: No change tracked"
-      return
-    end
-
-    if @event.is_published?
-      # Don't let users see who this is mailed to
-      subscribers = @event.votes_for.up.by_type(User).voters
-      subscribers.each do |user|
-        @user = user
-        mail(
-          to: mail_to(user),
-          bcc: mail_bcc,
-          subject: 'Conferina event has been updated'
-        )
-      end
-
-      if subscribers.empty?
-        puts "Event Updated. Subscriptions not sent: No subscribers"
-      end
+      error_message = "Event Updated. Subscriptions not sent: No Changes"
     else
-      puts "Event Updated. Subscriptions not sent: Not published"
+      @tracked_changes_definitions = [
+        # Field Display name           Field Name     Field value
+        ['Description',                :description,  @event.description],
+        ['Call For Papers Due Date',   :call_for,     @event.call_for],
+        ['Paper Accepted Notify Date', :notification, @event.notification],
+        ['Paper Submission Deadline',  :submission,   @event.submission],
+        ['Category',                   :category,     @event.category.category],
+        ['Start Date',                 :start_date,   @event.start_date],
+        ['End Date',                   :end_date,     @event.end_date],
+        ['Address',                    :address,      @event.address],
+      ]
+      tracked_fields = @tracked_changes_definitions.map{ |definition| definition[1] }
+
+      if (tracked_fields & @changes.keys).empty?
+        error_message = "Event Updated. Subscriptions not sent: No tracked changes"
+      end
+
+      if @event.is_published?
+        # Don't let users see who this is mailed to
+        subscribers = @event.votes_for.up.by_type(User).voters
+
+        if subscribers.empty?
+          error_message = "Event Updated. Subscriptions not sent: No subscribers"
+        end
+
+        subscribers.each do |user|
+          @user = user
+          mail(
+            to: mail_to(user),
+            bcc: mail_bcc,
+            subject: 'Conferina event has been updated'
+          )
+        end
+      else
+        error_message = "Event Updated. Subscriptions not sent: Not published"
+      end
     end
 
+    puts error_message unless error_message.nil?
+    true # Don't rollback
   end
+
 
 end

4d7848b8c02838db5fe7ded3c0e66a98b028ae7e
Mykl Clason <myklclason@gmail.com>
2017-03-29 03:50:31 +0000

    Fix handling of tracked changes

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -17,9 +17,26 @@ class Notifier < ApplicationMailer
 
     if @changes.empty?
       puts "Event Updated. Subscriptions not sent: No subscribers"
+      return
     end
 
-    return if @changes.empty?
+    @tracked_changes_definitions = [
+      # Field Display name           Field Name     Field value
+      ['Description',                :description,  @event.description],
+      ['Call For Papers Due Date',   :call_for,     @event.call_for],
+      ['Paper Accepted Notify Date', :notification, @event.notification],
+      ['Paper Submission Deadline',  :submission,   @event.submission],
+      ['Category',                   :category,     @event.category.category],
+      ['Start Date',                 :start_date,   @event.start_date],
+      ['End Date',                   :end_date,     @event.end_date],
+      ['Address',                    :address,      @event.address],
+    ]
+    tracked_fields = @tracked_changes_definitions.map{ |definition| definition[1] }
+
+    if (tracked_fields & @changes.keys).empty?
+      puts "Event Updated. Subscriptions not sent: No change tracked"
+      return
+    end
 
     if @event.is_published?
       # Don't let users see who this is mailed to
